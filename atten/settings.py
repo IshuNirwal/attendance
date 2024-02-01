27b7d30af88fb81c7fed58cd4ec8f1fc2dd80d66
Ishu Nirwal <ishunirwal2@gmail.com>
2024-02-01 12:12:42 +0530

    add corsheader

--- atten/settings.py
+++ atten/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-oul7o9f3b^&-o=1re%1vp@$)9a24^2i+u+l3o$kwe=&0u-_x&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['65.0.87.100', 'localhost:3000']
 
 
 # Application definition
