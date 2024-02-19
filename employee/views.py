ec80edb392233fe82204bd67e4f238dcffe87b29
Ishu Nirwal <ishunirwal2@gmail.com>
2024-02-19 17:07:47 +0530

    some change

--- employee/views.py
+++ employee/views.py
@@ -7,7 +7,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from datetime import datetime
 
-
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
 
